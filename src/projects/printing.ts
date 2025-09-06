533d4ef80ce89e0c66e6aa5f8d37a339715e51d6
Ron Leisti <ron@ronleisti.online>
2025-09-06 17:51:36 +0000

    fix(projects): add formatting to the project summary report

--- src/projects/printing.ts
+++ src/projects/printing.ts
@@ -1,21 +1,34 @@
 import { ProjectSummary } from "./ProjectDefinition";
 import { formatDay } from "../journal/util";
+import { styleText } from "node:util";
 
 export function printProjectSummary(
   summary: ProjectSummary,
   clientId: string,
   projectId: string,
 ): void {
-  console.log(`Project Summary: ${clientId}/${projectId}`);
+  console.log(
+    "Project Summary: " + styleText(["bold"], `${clientId}/${projectId}`),
+  );
   console.log("=====================================");
   console.log();
 
-  console.log(`Status: ${formatProjectStatus(summary.status)}`);
-  console.log(`Total Estimated Days: ${summary.totalEstimatedDays.toFixed(1)}`);
   console.log(
-    `Estimated Completion: ${formatDay(summary.estimatedCompletionDate)}`,
+    styleText(["bold"], "Status: ") + formatProjectStatus(summary.status),
   );
-  console.log(`Completion Progress: ${summary.completionPercentage}%`);
+  console.log(
+    styleText(["bold"], "Total Estimated Days: ") +
+      summary.totalEstimatedDays.toFixed(1),
+  );
+  console.log(
+    styleText(["bold"], "Estimated Completion: ") +
+      formatDay(summary.estimatedCompletionDate),
+  );
+  console.log(
+    styleText(["bold"], "Completion Progress: ") +
+      summary.completionPercentage +
+      "%",
+  );
 }
 
 function formatProjectStatus(status: string): string {
