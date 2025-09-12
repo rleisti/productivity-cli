6a410a200bba2c22ce92739c12f4260fbee2f32b
Ron Leisti <ron@ronleisti.online>
2025-09-12 00:11:03 +0000

    refactor(projects): Refactoring ProjectSimulation

--- src/projects/ProjectSimulation.ts
+++ src/projects/ProjectSimulation.ts
@@ -54,29 +54,14 @@ export class ProjectSimulation {
 
   public run(): SimulatedProject {
     const checkpoints = new Array<Checkpoint>();
-    checkpoints.push({
-      id: 0,
-      day: this.project.admin.start_date,
-      freePeople: Object.keys(this.project.admin.person),
-      completedTasks: [],
-      incoming: [],
-      outgoing: [],
-    });
-    let nextCheckpointId = 1;
-
-    const currentPersonCheckpoints = new Map<string, Checkpoint>();
-    this.personIds.forEach((personId) => {
-      currentPersonCheckpoints.set(personId, checkpoints[0]);
-    });
-
-    const taskCheckpoints = new Map<string, Array<Checkpoint>>();
-    this.taskIds.forEach((taskId) => {
-      taskCheckpoints.set(taskId, new Array<Checkpoint>());
-    });
-
+    this.createFirstCheckpoint(checkpoints);
+    const currentPersonCheckpoints =
+      this.initializePersonCheckpoints(checkpoints);
+    const taskCheckpoints = this.initializeTaskCheckpoints();
     const floats = this.calculateFloats(); // TODO: This may need to be re-evaluated after each checkpoint
     const incompleteTasks = Array.from(this.taskIds);
 
+    let nextCheckpointId = 1;
     let iterationCounter = 0;
     while (incompleteTasks.length > 0 && iterationCounter < 1000) {
       iterationCounter++;
@@ -86,137 +71,203 @@ export class ProjectSimulation {
         floats,
       );
       let advancedATask = false;
-      taskLoop: for (const taskId of sortedIncompleteTasks) {
-        const task = this.project.tasks[taskId];
-        for (const checkpoint of checkpoints) {
-          const hasAllDependencies = task.dependencies.every((dep) =>
-            checkpoint.completedTasks.includes(dep),
-          );
-          if (hasAllDependencies) {
-            const outcome = this.simulateTask(
-              taskId,
-              checkpoint.day,
-              checkpoint.freePeople,
-            );
-            if (outcome != null) {
-              const newCheckpoint: Checkpoint = {
-                id: nextCheckpointId++,
-                day: outcome.endDay,
-                freePeople: [outcome.personId],
-                completedTasks: checkpoint.completedTasks.concat(taskId),
-                incoming: [],
-                outgoing: [],
-              };
-              const execution: TaskExecution = {
-                ...outcome,
-                float: floats.get(taskId)!,
-                estimate: calculateTaskEstimate(task.estimate_days),
-                from: checkpoint.id,
-                to: newCheckpoint.id,
-              };
-              newCheckpoint.incoming.push(execution);
-              checkpoint.outgoing.push(execution);
-              checkpoints.push(newCheckpoint);
-              currentPersonCheckpoints.set(outcome.personId, newCheckpoint);
-              newCheckpoint.completedTasks.forEach((taskId) => {
-                taskCheckpoints.get(taskId)!.push(newCheckpoint);
-              });
-              incompleteTasks.splice(incompleteTasks.indexOf(taskId), 1);
-              advancedATask = true;
-              break taskLoop;
-            }
-          }
-        }
-      }
+      const advanceResult = this.advanceNextTask(
+        sortedIncompleteTasks,
+        checkpoints,
+        nextCheckpointId,
+        floats,
+        currentPersonCheckpoints,
+        taskCheckpoints,
+        incompleteTasks,
+        advancedATask,
+      );
+      nextCheckpointId = advanceResult.nextCheckpointId;
+      advancedATask = advanceResult.advancedATask;
 
       if (!advancedATask) {
-        for (const taskId of sortedIncompleteTasks) {
-          const task = this.project.tasks[taskId];
-          const isTaskReady = task.dependencies.every(
-            (dependency) => taskCheckpoints.get(dependency)!.length > 0,
-          );
-          if (!isTaskReady) {
-            continue;
-          }
-
-          const ownerCheckpoint = task.owners
-            .map((ownerId) => ({
-              owner: ownerId,
-              checkpoint: currentPersonCheckpoints.get(ownerId)!,
-            }))
-            .sort((a, b) => compareDays(a.checkpoint.day, b.checkpoint.day))[0];
-          if (!ownerCheckpoint.checkpoint) {
-            continue;
-          }
-
-          const newCheckpoint: Checkpoint = {
-            id: nextCheckpointId++,
-            day: ownerCheckpoint.checkpoint.day,
-            freePeople: [ownerCheckpoint.owner],
-            completedTasks: Array.from(
-              ownerCheckpoint.checkpoint.completedTasks,
-            ),
-            incoming: [],
-            outgoing: [],
-          };
-          const waitEvent: TaskExecution = {
-            from: ownerCheckpoint.checkpoint.id,
-            to: newCheckpoint.id,
-            personId: ownerCheckpoint.owner,
-            startDay: ownerCheckpoint.checkpoint.day,
-            endDay: ownerCheckpoint.checkpoint.day,
-          };
-          newCheckpoint.incoming.push(waitEvent);
-          ownerCheckpoint.checkpoint.outgoing.push(waitEvent);
-          checkpoints.push(newCheckpoint);
-          currentPersonCheckpoints.set(ownerCheckpoint.owner, newCheckpoint);
-
-          let iterationCounter2 = 0;
-          let missingDependencies = task.dependencies.filter(
-            (dependency) => !newCheckpoint.completedTasks.includes(dependency),
-          );
-          while (missingDependencies.length > 0 && iterationCounter2 < 100) {
-            iterationCounter2++;
-
-            const dependency = missingDependencies[0];
-            const sourceCheckpoint = taskCheckpoints.get(dependency)![0];
-            const [earliestDay, latestDay] =
-              compareDays(newCheckpoint.day, sourceCheckpoint.day) < 0
-                ? [newCheckpoint.day, sourceCheckpoint.day]
-                : [sourceCheckpoint.day, newCheckpoint.day];
-            const sourceWaitEvent: TaskExecution = {
-              from: sourceCheckpoint.id,
-              to: newCheckpoint.id,
-              startDay: earliestDay,
-              endDay: latestDay,
-            };
-            newCheckpoint.day = latestDay;
-            newCheckpoint.incoming.push(sourceWaitEvent);
-            newCheckpoint.completedTasks = newCheckpoint.completedTasks.concat(
-              sourceCheckpoint.completedTasks.filter(
-                (task) => !newCheckpoint.completedTasks.includes(task),
-              ),
-            );
-            sourceCheckpoint.outgoing.push(sourceWaitEvent);
-
-            missingDependencies = task.dependencies.filter(
-              (dependency) =>
-                !newCheckpoint.completedTasks.includes(dependency),
-            );
-          }
-
-          newCheckpoint.completedTasks.forEach((taskId) => {
-            taskCheckpoints.get(taskId)!.push(newCheckpoint);
-          });
-
-          break;
-        }
+        nextCheckpointId = this.waitForDependencies(
+          sortedIncompleteTasks,
+          taskCheckpoints,
+          currentPersonCheckpoints,
+          nextCheckpointId,
+          checkpoints,
+        );
       }
     }
 
     return {
       checkpoints: this.optimizeCheckpoints(checkpoints),
     };
+  }
+
+  private initializeTaskCheckpoints() {
+    const taskCheckpoints = new Map<string, Array<Checkpoint>>();
+    this.taskIds.forEach((taskId) => {
+      taskCheckpoints.set(taskId, new Array<Checkpoint>());
+    });
+    return taskCheckpoints;
+  }
+
+  private initializePersonCheckpoints(checkpoints: Checkpoint[]) {
+    const currentPersonCheckpoints = new Map<string, Checkpoint>();
+    this.personIds.forEach((personId) => {
+      currentPersonCheckpoints.set(personId, checkpoints[0]);
+    });
+    return currentPersonCheckpoints;
+  }
+
+  private createFirstCheckpoint(checkpoints: Checkpoint[]) {
+    checkpoints.push({
+      id: 0,
+      day: this.project.admin.start_date,
+      freePeople: Object.keys(this.project.admin.person),
+      completedTasks: [],
+      incoming: [],
+      outgoing: [],
+    });
+  }
+
+  private advanceNextTask(
+    sortedIncompleteTasks: Array<string>,
+    checkpoints: Checkpoint[],
+    nextCheckpointId: number,
+    floats: Map<string, number>,
+    currentPersonCheckpoints: Map<string, Checkpoint>,
+    taskCheckpoints: Map<string, Array<Checkpoint>>,
+    incompleteTasks: string[],
+    advancedATask: boolean,
+  ) {
+    taskLoop: for (const taskId of sortedIncompleteTasks) {
+      const task = this.project.tasks[taskId];
+      for (const checkpoint of checkpoints) {
+        const hasAllDependencies = task.dependencies.every((dep) =>
+          checkpoint.completedTasks.includes(dep),
+        );
+        if (hasAllDependencies) {
+          const outcome = this.simulateTask(
+            taskId,
+            checkpoint.day,
+            checkpoint.freePeople,
+          );
+          if (outcome != null) {
+            const newCheckpoint: Checkpoint = {
+              id: nextCheckpointId++,
+              day: outcome.endDay,
+              freePeople: [outcome.personId],
+              completedTasks: checkpoint.completedTasks.concat(taskId),
+              incoming: [],
+              outgoing: [],
+            };
+            const execution: TaskExecution = {
+              ...outcome,
+              float: floats.get(taskId)!,
+              estimate: calculateTaskEstimate(task.estimate_days),
+              from: checkpoint.id,
+              to: newCheckpoint.id,
+            };
+            newCheckpoint.incoming.push(execution);
+            checkpoint.outgoing.push(execution);
+            checkpoints.push(newCheckpoint);
+            currentPersonCheckpoints.set(outcome.personId, newCheckpoint);
+            newCheckpoint.completedTasks.forEach((taskId) => {
+              taskCheckpoints.get(taskId)!.push(newCheckpoint);
+            });
+            incompleteTasks.splice(incompleteTasks.indexOf(taskId), 1);
+            advancedATask = true;
+            break taskLoop;
+          }
+        }
+      }
+    }
+    return { nextCheckpointId, advancedATask };
+  }
+
+  private waitForDependencies(
+    sortedIncompleteTasks: Array<string>,
+    taskCheckpoints: Map<string, Array<Checkpoint>>,
+    currentPersonCheckpoints: Map<string, Checkpoint>,
+    nextCheckpointId: number,
+    checkpoints: Checkpoint[],
+  ) {
+    for (const taskId of sortedIncompleteTasks) {
+      const task = this.project.tasks[taskId];
+      const isTaskReady = task.dependencies.every(
+        (dependency) => taskCheckpoints.get(dependency)!.length > 0,
+      );
+      if (!isTaskReady) {
+        continue;
+      }
+
+      const ownerCheckpoint = task.owners
+        .map((ownerId) => ({
+          owner: ownerId,
+          checkpoint: currentPersonCheckpoints.get(ownerId)!,
+        }))
+        .sort((a, b) => compareDays(a.checkpoint.day, b.checkpoint.day))[0];
+      if (!ownerCheckpoint.checkpoint) {
+        continue;
+      }
+
+      const newCheckpoint: Checkpoint = {
+        id: nextCheckpointId++,
+        day: ownerCheckpoint.checkpoint.day,
+        freePeople: [ownerCheckpoint.owner],
+        completedTasks: Array.from(ownerCheckpoint.checkpoint.completedTasks),
+        incoming: [],
+        outgoing: [],
+      };
+      const waitEvent: TaskExecution = {
+        from: ownerCheckpoint.checkpoint.id,
+        to: newCheckpoint.id,
+        personId: ownerCheckpoint.owner,
+        startDay: ownerCheckpoint.checkpoint.day,
+        endDay: ownerCheckpoint.checkpoint.day,
+      };
+      newCheckpoint.incoming.push(waitEvent);
+      ownerCheckpoint.checkpoint.outgoing.push(waitEvent);
+      checkpoints.push(newCheckpoint);
+      currentPersonCheckpoints.set(ownerCheckpoint.owner, newCheckpoint);
+
+      let iterationCounter2 = 0;
+      let missingDependencies = task.dependencies.filter(
+        (dependency) => !newCheckpoint.completedTasks.includes(dependency),
+      );
+      while (missingDependencies.length > 0 && iterationCounter2 < 100) {
+        iterationCounter2++;
+
+        const dependency = missingDependencies[0];
+        const sourceCheckpoint = taskCheckpoints.get(dependency)![0];
+        const [earliestDay, latestDay] =
+          compareDays(newCheckpoint.day, sourceCheckpoint.day) < 0
+            ? [newCheckpoint.day, sourceCheckpoint.day]
+            : [sourceCheckpoint.day, newCheckpoint.day];
+        const sourceWaitEvent: TaskExecution = {
+          from: sourceCheckpoint.id,
+          to: newCheckpoint.id,
+          startDay: earliestDay,
+          endDay: latestDay,
+        };
+        newCheckpoint.day = latestDay;
+        newCheckpoint.incoming.push(sourceWaitEvent);
+        newCheckpoint.completedTasks = newCheckpoint.completedTasks.concat(
+          sourceCheckpoint.completedTasks.filter(
+            (task) => !newCheckpoint.completedTasks.includes(task),
+          ),
+        );
+        sourceCheckpoint.outgoing.push(sourceWaitEvent);
+
+        missingDependencies = task.dependencies.filter(
+          (dependency) => !newCheckpoint.completedTasks.includes(dependency),
+        );
+      }
+
+      newCheckpoint.completedTasks.forEach((taskId) => {
+        taskCheckpoints.get(taskId)!.push(newCheckpoint);
+      });
+
+      break;
+    }
+    return nextCheckpointId;
   }
 
   private sortedTasksByFloats(
